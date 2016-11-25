199054bc60474f4e6d8e06dc09af976be07283e5
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-11-25 16:44:28 +0900

    implement Pug at GitHub sample

---
+++ src/js/page/PugGithub.js
@@ -0,0 +1,47 @@
+import ns from '../module/ns';
+import Htjs from '../module/Htjs';
+
+export default class PugGithub {
+  constructor(opts = {}) {
+    this.initialize();
+  }
+
+  initialize() {
+    this.htjs = new Htjs({
+      prefixStr: '',
+    });
+
+document.querySelector('html').appendChild(
+    html({lang: "ja"})(
+      head(
+        title("Htjs"),
+        script({type: "text/javascript"})(
+          "if (foo) bar(1 + 5)"
+        )
+      ),
+      body(
+        h1("Htjs - node template engine"),
+        div({id: "container", class: "col"})(
+          p("すごいぞ！"),
+          p("Htjsはかんたんできょうりょくなテンプレートエンジンだぞ！")
+        )
+      )
+    ).elm
+)
+    // document.querySelector('html').innerHTML = html({lang: "ja"})(
+    //   head(
+    //     title("Htjs"),
+    //     script({type: "text/javascript"})(
+    //       "if (foo) bar(1 + 5)"
+    //     )
+    //   ),
+    //   body(
+    //     h1("Htjs - node template engine"),
+    //     div({id: "container", class: "col"})(
+    //       p("すごいぞ！"),
+    //       p("Htjsはかんたんできょうりょくなてんぷれーとえんじんだぞ！")
+    //     )
+    //   )
+    // );
+  }
+}
