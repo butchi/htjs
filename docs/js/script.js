2682109068f99e39b7863e0e636fe0d8537e631a
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2016-11-30 00:02:48 +0900

    build

--- docs/js/script.js
+++ docs/js/script.js
@@ -502,7 +502,11 @@ var Index = function () {
         prefixStr: ''
       });
 
-      document.querySelector('.wrapper').append(div(div({ class: "test" })(h1("HTJS(仮)"), p({ class: "content" })("これは", a({ href: "http://example.com", target: "_blank" })("アンカー"), "の", br(), span("テ", "ス", "ト"), "です。"), span({
+      var wrapperElm = document.createElement('div');
+      wrapperElm.classList.add('wrapper');
+      document.querySelector('body').append(wrapperElm);
+
+      wrapperElm.append(div(div({ class: "test" })(h1("HTJS(仮)"), p({ class: "content" })("これは", a({ href: "http://example.com", target: "_blank" })("アンカー"), "の", br(), span("テ", "ス", "ト"), "です。"), span({
         style: {
           "color": "#f00",
           "font-weight": "bold",
